b52a2ae1a009bb167b449de72d45f993347289a3
vickychandra357 <81799094+vickychandra357@users.noreply.github.com>
2023-01-15 21:55:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function threeSum(arr, target) {
   //your code here
-}
+     
+    // To store the closest sum
+    let closestSum = Number.MAtarget_VALUE;
+ 
+    // Run three nested loops each loop
+    // for each element of triplet
+    for(let i = 0; i < arr.length ; i++)
+    {
+        for(let j =i + 1; j < arr.length; j++)
+        {
+            for(let k =j + 1; k < arr.length; k++)
+            {
+                 
+                // Update the closestSum
+                if (Math.abs(target - closestSum) >
+                    Math.abs(target - (arr[i] + arr[j] + arr[k])))
+                    closestSum = (arr[i] + arr[j] + arr[k]);
+            }
+        }
+    }
 
-module.exports = threeSum;
+module.etargetports = threeSum;
